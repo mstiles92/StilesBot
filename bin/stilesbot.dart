182df11c0478d54a13b4c8dd1be778532a79e7ee
mstiles92 <mstiles92@gmail.com>
2014-10-31 18:21:07 -0400

    Add easy way to register and handle commands.

--- bin/stilesbot.dart
+++ bin/stilesbot.dart
@@ -1,15 +1,24 @@
 import 'package:irc/irc.dart';
 import 'package:intl/intl.dart';
 import 'dart:io';
+import 'dart:async';
+
+typedef CommandHandler(CommandEvent event);
 
 class StilesBot extends Bot {
     Client _client;
     List<String> channels = [];
     Directory _logLocation;
     Directory _channelLogs;
+
+    Map<String, StreamController<CommandEvent>> commands = {};
+    CommandHandler commandNotFound = (event) => null;
+    String commandPrefix;
+    Iterable<String> commandNames() => commands.keys;
+
     Client get client => _client;
 
-    StilesBot(BotConfig config, String logDirectory) {
+    StilesBot(BotConfig config, {String logDirectory: "logs", this.commandPrefix: "!"}) {
         _client = new Client(config);
         _logLocation = new Directory(logDirectory);
         _channelLogs = new Directory("${logDirectory}/channels");
@@ -43,6 +52,8 @@ class StilesBot extends Bot {
             print("<${event.target}>${line}");
             log(line, channel: event.channel);
         });
+
+        register(handleAsCommand);
     }
 
     void log(String message, {Channel channel}) {
@@ -60,5 +71,29 @@ class StilesBot extends Bot {
         }
 
         file.writeAsString(message + "\n", mode: FileMode.APPEND);
+    }
+
+    void handleAsCommand(MessageEvent event) {
+        String message = event.message;
+
+        if (message.startsWith(commandPrefix)) {
+            var end = message.contains(" ") ? message.indexOf(" ", commandPrefix.length) : message.length;
+            var command = message.substring(commandPrefix.length, end);
+            var args = message.substring(end != message.length ? end + 1 : end).split(" ");
+
+            args.removeWhere((arg) => arg.isEmpty || arg == " ");
+
+            if (commands.containsKey(command)) {
+                commands[command].add(new CommandEvent(event, command, args));
+            } else {
+                commandNotFound(new CommandEvent(event, command, args));
+            }
+        }
+    }
+
+    void registerCommand(String name, CommandHandler handler) {
+        commands.putIfAbsent(name, () {
+            return new StreamController.broadcast();
+        }).stream.listen(handler);
     }
 }
